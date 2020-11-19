80ccf58d71cebec50050b7199e721ab95aafe014
Simon Mazzucca <simonmazzucca@hotmail.com>
2020-11-19 17:16:09 -0500

    TestLoopsAndIterables added some good shit

--- UnitTests/test_loops_and_iterables.py
+++ UnitTests/test_loops_and_iterables.py
@@ -3,6 +3,17 @@ import unittest
 
 class TestLoopsAndIterables(unittest.TestCase):
 
+    def test_iterator(self):
+        nums = [1, 2, 3, 4, 5, 6]
+        iterator = iter(nums)
+
+        while True:
+            try:
+                next_element = next(iterator)
+                print(next_element)
+            except StopIteration:
+                break
+
     def test_for_loop(self):
         a = [4, 5, 6]
         self.assertEqual(True, 5 in a)
@@ -22,6 +33,20 @@ class TestLoopsAndIterables(unittest.TestCase):
         for i in range(len(nums)):
             print(i)
 
+        print("for loop with tuple collection")
+        for i, j in [(1, 2), (3, 4), (5, 6)]:
+            print(i, j)
+
+        print("for loop with HT key/value, using dic.items()")
+        dic = {'foo': 1, 'bar': 2, 'baz': 3}
+        for k, v in dic.items():
+            print(k, v)
+
+        print("for loop with index and item, using enumerate(lists)")
+        lists = [[1, 2, 3], [4, 5], [6]]
+        for index, item in enumerate(lists):
+            print(index, item)
+
     def test_while_loop(self):
         i = 0
 
@@ -30,5 +55,3 @@ class TestLoopsAndIterables(unittest.TestCase):
             i = i + 1
             if i == 4:
                 break
-
-
