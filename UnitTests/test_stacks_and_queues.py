dc52e66cc40057fc8d03c5df9ebcab88ce9dcab7
Simon Mazzucca <simonmazzucca@hotmail.com>
2021-02-18 08:24:34 -0500

    TestStackAndQueues

--- UnitTests/test_stacks_and_queues.py
+++ UnitTests/test_stacks_and_queues.py
@@ -1,11 +1,18 @@
 import unittest
 
-
+# Stacks and Queues are almost identical in Python
+# append() is used for both PUSH and ENQUEUE, but...
+#
+# Stack:
+# To POP last added item: pop()
+#
+# Queue:
+# To DEQUEUE first added item: pop(0)
 class TestStackAndQueues(unittest.TestCase):
 
     def test_stack(self):
 
-        # push
+        # Push
         stack = []
         stack.append(1)
         stack.append(2)
@@ -18,6 +25,25 @@ class TestStackAndQueues(unittest.TestCase):
 
         # Pop
         pop_val = stack.pop()
-        self.assertEqual(3, peek_val)
+        self.assertEqual(3, pop_val)
         self.assertEqual(2, len(stack))
+
+
+    def test_queue(self):
+
+        # Enqueue
+        queue = []
+        queue.append(1)
+        queue.append(2)
+        queue.append(3)
+        self.assertEqual(3, len(queue))
+
+        # No peek... do it manually
+        peek_val = queue[-1]
+        self.assertEqual(3, peek_val)
+
+        # Dequeue
+        dequeue_val = queue.pop(0)
+        self.assertEqual(1, dequeue_val)
+        self.assertEqual(2, len(queue))
 
